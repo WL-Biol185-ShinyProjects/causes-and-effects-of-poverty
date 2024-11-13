219710ebe693a8bd70ad404c1138744b997ce222
quinlinzunk <zunkq25@mail.wlu.edu>
2024-11-13 16:51:19 -0500

    finished putting research info into sidepanels and interactive drop down on OPM vs SPM

--- server.r
+++ server.r
@@ -1,14 +1,10 @@
 library(shiny)
 
 # where the data is manipulated and visualizations are prepared
-server <- function(input, output, session) {
+function(input, output, session) {
+  
   output$examplePlot <- renderPlot({
     
   })
 }
 
-# where data is shown and visualizations are served
-ui = fluidPage("The Causes and Consequences of Poverty")
-
-#Launch the codes
-shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -50,7 +50,8 @@ navbarPage(theme = shinytheme("united"),
                           and income opportunities."),
                         p("Environmental Factors: Low-income groups are more susceptible to unsafe housing, 
                         polluted neighborhoods, and vulnerability to climate-related issues. This could cause a lack 
-                        of access to clean air, water, and safe living conditions which can further deepen the poverty cycle.")
+                        of access to clean air, water, and safe living conditions which can further deepen the poverty 
+                          cycle.")
                         
                       ),
                       mainPanel(
@@ -58,7 +59,79 @@ navbarPage(theme = shinytheme("united"),
                         p("This is where main content like plots or tables will appear")
                       )
                     )),
-           tabPanel("OPM vs SPM"),
-           tabPanel("Education"),
-           tabPanel("Demographics")
+           tabPanel("OPM vs SPM",
+                    h1("Official Poverty Measure vs. Supplemental Poverty Measure"),
+                    sidebarLayout(
+                      sidebarPanel(
+                        fluidRow(
+                          column(12,
+                        selectInput("dataset", "Choose a Measurement:",
+                                    choices = c("OPM", "SPM")),
+                        
+                        selectInput("dataset", "Choose a Difference:",
+                                    choices = c("Measurement Units", "Poverty Threshold", "Threshold Adjustments",
+                                                "Resource Measure", "Universe", "Time Series")),
+                        
+                        actionButton("update", "Update View")
+                      ),
+                      column(12,
+                             h4("Results will be here!!!!")
+                             ),
+                      column(12,
+                             p("The OPM does not consider geographic variations or effects of government 
+                             assistance programs. Policies based on the OPM could overlook specific needs such as 
+                             differing costs of living across regions potentially leading to underfunding or misdirected 
+                             resources."),
+                             p("The SPM allows a more comprehensive view by including both cash income and non-cash 
+                             benefits as well as necessary expenses such as taxes, medical costs, and work related expenses.
+                             Policies based on the SPM have a clearer picture of the real resources available to individuals 
+                             and can better assess the impact of government programs on poverty rates.")
+                             )
+                      )
+                      ),
+                      mainPanel(
+                        h3("Main Panel"),
+                        p("This is where main content like plots or tables will appear")
+                      )
+                    )),
+           tabPanel("Education",
+                    h1("Education"),
+                    sidebarLayout(
+                      sidebarPanel(
+                        p("Education and poverty are closely related as they both influence the other. Education can 
+                        help increase employment opportunities as it provides the necessary skills valued in the labor 
+                        market. Higher the level of education most times can ensure a stable job with higher earnings, 
+                        reducing an individual’s risk of poverty. As we will discuss more in the health care tab, education
+                        level often influences health outcomes as informed individuals are more likely to make informed 
+                        decisions and have more access to healthy foods and environments. Education also extends across 
+                        generations causing educated parents to more likely support the education of their children. This 
+                        is why cycles of poverty are so difficult to break.")
+                    ),
+                    mainPanel(
+                      h3("Main Panel"),
+                      p("This is where laetitias amazing graphs will gooooo")
+                    )
+                    
+                    )),
+           tabPanel("Demographics",
+                    h1("Demographics"),
+                    sidebarLayout(
+                      sidebarPanel(
+                        p("Demographics - factors such as age, race, gender, family structure, and geographic location - 
+                        have a significant impact on poverty levels and can shape individuals’ economic opportunities, 
+                        health, and overall well-being. The effects of demographics can influence the likelihood of one 
+                        experiencing poverty as well as the barriers faced when trying to escape it. Children are one of 
+                        the most affected groups when it comes to poverty because growing up in poverty can influence 
+                        development, nutrition, education, healthy living conditions, and much more. Historical discrimination
+                        like redlining, exclusion from labor markets, and education inequalities have left lasting impacts on
+                        wealth and income for communities of color. The wage gap contributes to higher poverty rates among 
+                        women, especially single mothers. And finally, geographic location has a large influence on poverty
+                        rates due to employment opportunities, healthcare, and education.")
+                      ),
+                      mainPanel(
+                        h3("Main Panel"),
+                        p("This is where laetitias amazing graphs will gooooo")
+                      )
+                    )
+                    )
            )
